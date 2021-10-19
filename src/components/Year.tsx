3ac4e8094146ba116f4b6138b872512c0c5a08a1
lucasd <Driscoll_Lucas@bah.com>
2021-10-18 20:55:19 -0400

    Merged with fixWarning

--- src/components/Year.tsx
+++ src/components/Year.tsx
@@ -6,66 +6,57 @@ import SemesterProps from "../interfaces/Semester";
 
 interface FullYearProps extends YearProps{
     handleSubmit: (event: FormEvent<HTMLFormElement>) => void,
-    handleInput: (event: ChangeEvent<HTMLInputElement>) => void
+    handleInput: (event: ChangeEvent<HTMLInputElement>) => void,
+    formUuid: string | null,
+    setFormUuid: (newId: string | null) => void
 }
 
-function Year(props: FullYearProps): JSX.Element{
-    const [viewing, setViewing] = useState<boolean>(false);
-    const [formOpen, setFormOpen] = useState<boolean>(false);
+const Year = React.forwardRef((props: FullYearProps, ref: React.ForwardedRef<HTMLDivElement>) => {
     const overlayButton = useRef(null);
     return (
-        <Container className="container-sm">
-            <Row>
-                <div>
-                    Year {props.index}
-                    <button onClick={() => {
-                        setViewing(!viewing);
-                    }
-                    }>{">"}</button>
-                </div>
-            </Row>
-            <Collapse in={viewing}>
-                <Row>
-                    {props.semesters.map((semester: SemesterProps) => {
-                        return (
-                            <Col key={semester.uuid}>{semester.name}</Col>
-                        );
-                    })}
-                    <Col>
-                        <button ref={overlayButton} onClick={() => {
-                            setFormOpen(!formOpen);
-                        }}>+</button>
-                        <Overlay target={overlayButton} placement="right-end" show={formOpen} onHide={() => {
-                            setFormOpen(false);
-                        }}rootClose={true}>
-                            <Popover id="popover-basic">
-                                <PopoverContent>
-                                    <form onSubmit={props.handleSubmit}>
-                                        <label>
-                                            season: 
-                                        </label>
-                                        <input type="text" name="season" onChange={props.handleInput}/>
-                                        <br/>
-                                        <label>
-                                            starts:
-                                        </label>
-                                        <input type="date" name="starts" onChange={props.handleInput}/>
-                                        <br/>
-                                        <label>
-                                            ends:
-                                        </label>
-                                        <input type="date" name="ends" onChange={props.handleInput}/>
-                                        <br/>
-                                        <input type="submit" value="submit" />
-                                    </form> </PopoverContent>
-                            </Popover>
-                        </Overlay>
-                    </Col>
-                </Row>
-            </Collapse>
-        </Container>
-
-        
+        <Container className="container-sm" ref={ref}>
+            <Col>
+                <Collapsible hidden={true} trigger={<button className="trigger">{`Year ${props.index} >`}</button>} transitionTime={200}>
+                    <Row>
+                        {sortedSemesters.map((semester: SemesterProps) => {
+                            return (
+                                <Col key={semester.uuid}>{semester.name}</Col>
+                            );
+                        })}
+                        <Col>
+                            <button className="trigger" ref={overlayButton} onClick={() => {
+                                props.setFormUuid(props.uuid === props.formUuid ? null : props.uuid);
+                            }}>+</button>
+                            <Overlay target={overlayButton} placement="right-end" show={props.formUuid === props.uuid} onHide={() => {
+                                props.setFormUuid(null);
+                            }}rootClose={true}>
+                                <Popover id="popover-basic">
+                                    <PopoverContent>
+                                        <form onSubmit={props.handleSubmit}>
+                                            <label>
+                                                season: 
+                                            </label>
+                                            <input type="text" name="season" onChange={props.handleInput}/>
+                                            <br/>
+                                            <label>
+                                                starts:
+                                            </label>
+                                            <input type="date" name="starts" onChange={props.handleInput}/>
+                                            <br/>
+                                            <label>
+                                                ends:
+                                            </label>
+                                            <input type="date" name="ends" onChange={props.handleInput}/>
+                                            <br/>
+                                            <input type="submit" value="submit" />
+                                        </form> </PopoverContent>
+                                </Popover>
+                            </Overlay>
+                        </Col>
+                    </Row>
+                </Collapsible>
+            </Col>
+        </Container>    
     );
 }
 
