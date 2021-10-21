88ce6632abd1230f29f9cf2cdbd6002a19428c05
lucasd <Driscoll_Lucas@bah.com>
2021-10-21 09:39:37 -0400

    fixed the last compiler warning

--- src/components/Scheduler.tsx
+++ src/components/Scheduler.tsx
@@ -9,42 +9,45 @@ interface SchedulerProps{
     json?: string
 }
 export function Scheduler(props: SchedulerProps): JSX.Element{
-    const years = useYears([{index: 1, uuid: uuid(), semesters: new Array<SemesterProps>()}]);
-    const [newName, setNewName] = useState<string | null>(null);
-    const [newStart, setNewStart] = useState<string | null>(null);
-    const [newEnd, setNewEnd] = useState<string | null>(null);
-    const [currentForm, setCurrentForm] = useState<string | null>(null);
-    const handleSemesterInput = (event: ChangeEvent<HTMLInputElement>) => {
-        switch(event.target.name){
-        case "season":
-            setNewName(event.target.value);
-            break;
-        case "starts":
-            setNewStart(event.target.value);
-            break;
-        case "ends":
-            setNewEnd(event.target.value);
-            break;
-        }
-    };
-    const handleSemesterSubmit = (event: FormEvent<HTMLFormElement>,id: string) => {
-        event.preventDefault();
-        years.putSemester(id, uuid(),new Date(newStart as string),new Date(newEnd as string),newName as string);
-    };
-    return (
-        <div>
-            {years.value.map((props: YearProps, i: number) => {
-                return (
-                    <div data-testid={"Year"} key={props.uuid}>
-                        <Year handleInput={handleSemesterInput} handleSubmit={(event: FormEvent<HTMLFormElement>) => {
-                            handleSemesterSubmit(event,props.uuid);
-                        }} semesters={props.semesters} uuid={props.uuid} index={i+1} formUuid={currentForm} setFormUuid={setCurrentForm}/>
-                    </div>
-                );
-            })}
-            <button data-testid="addYearButton" onClick={() => {
-                years.push(uuid(),years.value.length);
-            }}>+</button>
-        </div>
-    );
+    if(props.csv === undefined && props.json === undefined){
+        const years = useYears([{index: 1, uuid: uuid(), semesters: new Array<SemesterProps>()}]);
+        const [newName, setNewName] = useState<string | null>(null);
+        const [newStart, setNewStart] = useState<string | null>(null);
+        const [newEnd, setNewEnd] = useState<string | null>(null);
+        const [currentForm, setCurrentForm] = useState<string | null>(null);
+        const handleSemesterInput = (event: ChangeEvent<HTMLInputElement>) => {
+            switch(event.target.name){
+            case "season":
+                setNewName(event.target.value);
+                break;
+            case "starts":
+                setNewStart(event.target.value);
+                break;
+            case "ends":
+                setNewEnd(event.target.value);
+                break;
+            }
+        };
+        const handleSemesterSubmit = (event: FormEvent<HTMLFormElement>,id: string) => {
+            event.preventDefault();
+            years.putSemester(id, uuid(),new Date(newStart as string),new Date(newEnd as string),newName as string);
+        };
+        return (
+            <div>
+                {years.value.map((props: YearProps, i: number) => {
+                    return (
+                        <div data-testid={"Year"} key={props.uuid}>
+                            <Year handleInput={handleSemesterInput} handleSubmit={(event: FormEvent<HTMLFormElement>) => {
+                                handleSemesterSubmit(event,props.uuid);
+                            }} semesters={props.semesters} uuid={props.uuid} index={i+1} formUuid={currentForm} setFormUuid={setCurrentForm}/>
+                        </div>
+                    );
+                })}
+                <button data-testid="addYearButton" onClick={() => {
+                    years.push(uuid(),years.value.length);
+                }}>+</button>
+            </div>
+        );
+    }
+    return <></>;
 }
