9d5570c43086009e534695efa48b5f7f1f81a6d1
Max <maxperezmas@gmail.com>
2021-10-28 00:04:46 -0400

    cleaned up master page and added switch

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router-dom";
 import { Scheduler } from "./components/Scheduler";
 import Plans from "./components/Plans";
+import { Button } from "react-bootstrap";
 //import ReactDOM from "react-dom";
 
 const users = [
@@ -21,12 +22,24 @@ const users = [
 ];
 
 const IndexPage = () => {
-    return <Plans/>;
+    return (
+        <div>
+            <h1 className="center">Master Plan View</h1>
+            <Plans />
+        </div>
+    );
     
 };
 
 const Plan = () => {
-    return <Scheduler/>;
+    return (
+        <>
+            <Link to="/">
+                <Button variant="secondary" >
+                    Home</Button></Link>
+            <Scheduler />
+        </>
+    );
 };
 
 const UsersPage = () => {
@@ -59,16 +72,14 @@ function App(): JSX.Element {
 
     return (
         <div className="container">
+            
             <Router>
-                <Link to ="/">Home Page</Link>
-                <Route exact path="/" component={IndexPage}/>
-                <Link to = "/plan">Plan</Link>
-                <Route exact path="/plan" component={Scheduler}/>
-                <Link to = "/users">Users</Link>
-                <Route exact path="/users" component={UsersPage}/>
-                <Route exact path="/users/:userId" component={UserPage}/>
+
+                <Switch>
+                    <Route exact path="/" component={IndexPage}></Route>
+                    <Route exact path="/plan" component={Plan}></Route>
+                </Switch>
             </Router>
-            <a href="/about">about with browser reload</a>
         </div>
     );
 }
