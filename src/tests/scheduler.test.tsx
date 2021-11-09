ab44b7da67430508d3eda64df6f4b7890da28de1
lucasd <Driscoll_Lucas@bah.com>
2021-11-09 13:53:26 -0500

    Fixed linting errors.

--- src/tests/scheduler.test.tsx
+++ src/tests/scheduler.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { findByTestId, fireEvent, getByTestId, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, getByTestId, render, screen, waitFor } from "@testing-library/react";
 
 import { act } from "react-dom/test-utils";
 import { Scheduler } from "../components/Scheduler";
@@ -167,5 +167,5 @@ describe("Scheduler", () => {
 
 
 
-    })
+    });
 });
