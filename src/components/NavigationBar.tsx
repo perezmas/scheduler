a7e9673a07e2f82bfefd7e979b3b1b25396ceac1
Amani Kiruga <amaniedu71@gmail.com>
2021-11-18 08:02:44 -0500

    Add navigation bar

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const NavigationBar = (): JSX.Element => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Fancy Scheduler</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/Requirements">
+                            Requirements
+                        </Nav.Link>
+                        <NavDropdown title="Resources" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="https://catalog.udel.edu/">
+                                Course Catalog
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="https://my.udel.edu/task/all/udsisstudent">
+                                UDSIS
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="https://www.cis.udel.edu/">
+                                CISC Department Page
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavigationBar;
