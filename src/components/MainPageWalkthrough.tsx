083dc06c3b1dd398ff8740da60ab127d4df089b2
Amani Kiruga <amaniedu71@gmail.com>
2021-11-19 22:10:58 -0500

    Change classname of last step to point to add course

--- src/components/MainPageWalkthrough.tsx
+++ src/components/MainPageWalkthrough.tsx
@@ -9,38 +9,51 @@ const MainPageWalkthrough: React.FunctionComponent = () => {
                 title: "Welcome to the UD CIS Scheduler!",
                 disableBeacon: true,
                 target: ".g-4",
-                content: "This is the main page where you can create, edit, duplicate, and delete individual degree plans.",
+                content:
+                    "This is the main page where you can create, edit, duplicate, and delete individual degree plans.",
             },
             {
                 title: "Add Degree Plans here!",
                 target: ".add-plan-card",
-                content: "This is where you can add a plan to start designing your own CIS degree plan.",
+                content:
+                    "This is where you can add a plan to start designing your own CIS degree plan.",
             },
             {
                 title: "Keep degree requirements up-to-date here!",
                 target: ".home-page-reqs",
-                content: "Here, you can modify the requirements needed to complete your custom degree plan.",
+                content:
+                    "Here, you can modify the requirements needed to complete your custom degree plan.",
             },
             {
                 title: "More resources available as well!",
                 target: ".home-page-resources",
-                content: "Helpful links where you can find the CIS Course Catalog, your UDSIS, and the CISC Department Page.",
+                content:
+                    "Helpful links where you can find the CIS Course Catalog, your UDSIS, and the CISC Department Page.",
             },
             {
                 title: "Good Luck!",
-                target: ".home-page-reqs",
+                target: ".add-plan-card",
                 content: "Add a plan to get started.",
             },
-        ]
+        ],
     });
 
     return (
         <>
             <div
                 id="text"
-                data-testid={"main-page-walkthrough-modal"} 
-                style={{ marginLeft: "2%", marginRight: "auto", padding: "0.5rem", color: "black", opacity: 0.55, cursor: "pointer"}} 
-                onClick={() => setJoyride({ ...joyride, run: !joyride.run })}><a>Tutorial</a>
+                data-testid={"main-page-walkthrough-modal"}
+                style={{
+                    marginLeft: "2%",
+                    marginRight: "auto",
+                    padding: "0.5rem",
+                    color: "black",
+                    opacity: 0.55,
+                    cursor: "pointer",
+                }}
+                onClick={() => setJoyride({ ...joyride, run: !joyride.run })}
+            >
+                <a>Tutorial</a>
             </div>
             <ReactJoyride
                 steps={joyride.steps}
@@ -61,8 +74,9 @@ const MainPageWalkthrough: React.FunctionComponent = () => {
                 }}
                 locale={{
                     last: "End Tour",
-                    skip: "Close Tour"
-                }} />
+                    skip: "Close Tour",
+                }}
+            />
         </>
     );
 };
